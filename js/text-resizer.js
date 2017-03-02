232fe78e5bf0f79b3735e01161d0c228059c7b9a
JoshHolmes90 <p09251054@my365.dmu.ac.uk>
2017-03-02 13:35:26 +0000

    Added font awesome (external icon css), created basic template and started text resizer.

---
+++ js/text-resizer.js
@@ -0,0 +1,11 @@
+/*
+    Text Resizer, changes the sizes of the text to allow for accessibility
+    Author, Joshua Holmes.
+*/
+
+//Setting the sizing values
+var sizes = [
+        p = ['1em', '2em', '3em'],
+        h1 = ['2em', '3em', '4em']
+];
+
