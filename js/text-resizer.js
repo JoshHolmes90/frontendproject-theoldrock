a373ffeb11425fdfdda3c9446d7b03740b4e5d75
JoshHolmes90 <p09251054@my365.dmu.ac.uk>
2017-03-06 16:34:29 +0000

    Finished re-sizer, added basic style in index html.

--- js/text-resizer.js
+++ js/text-resizer.js
@@ -1,11 +1,33 @@
-/*
-    Text Resizer, changes the sizes of the text to allow for accessibility
-    Author, Joshua Holmes.
-*/
+/**
+    Changes the size of the font of all elements within the webpage
+    NOTE: Requires jQuery
+    Author: Joshua Holmes
+**/
+
+//Setting the current Index..
+var currentIndex = 1;
 
 //Setting the sizing values
 var sizes = [
-        p = ['1em', '2em', '3em'],
-        h1 = ['2em', '3em', '4em']
+    ["p", "1em", "2em", "3em"],
+    ["h1", "2em", "3em", "4em"]
 ];
+
+
+function trChangeSize() {
+    
+    //Set the main index for the medium size (default)...
+    if(currentIndex == 3) {
+        currentIndex = 1;
+    } else {
+        currentIndex++;
+    }
+    
+    //Loop through all the elements and change the font sizes...
+    sizes.forEach( function(s) {
+        $(s[0]).css("font-size", s[currentIndex]); 
+    });
+    
+    
+}
 
